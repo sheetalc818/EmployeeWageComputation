37388e1dcf603394493b82ccf1b448e3b82b38df
sheetalc818 <sheetalc818@gmail.com>
2019-12-24 17:02:06 +0530

    Added : Check whether Emp present

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,3 +2,19 @@
 
 #Print Welcome message
 echo "-----Welcome To Employee wage Computation-----"
+
+#---Checking for Employee Present or not---
+
+#Check Emp
+checkEmp=$((RANDOM%2))
+
+#Variables
+isPresent=1;
+
+#---Calculating Employee Daily Wage who present---
+if [ $checkEmp -eq $isPresent ]
+then
+   echo "Employee is Present"
+else
+   echo "Employee is Absent"
+fi
