3a3063f50f00a60832770951622035ddba3c4368
sheetalc818 <sheetalc818@gmail.com>
2020-01-07 12:45:35 +0530

    Added : Storing Daily wage along with Total wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,7 +3,7 @@
 #CONSTANTS FOR THE PROGRAMS
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
-MAX_HRS_IN_MONTH=10;
+MAX_HRS_IN_MONTH=100;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
@@ -11,21 +11,29 @@ NUM_WORKING_DAYS=20;
 totalEmpHr=0;
 totalWorkingDays=0;
 
-#FUNCTION
+#FUNCTION-To get Working hrs
 getWorkingHours() 
 {
 	case $1 in
 				$IS_FULL_TIME)
-					workHours=8
-					;;
+								workHours=8
+								;;
 				$IS_PART_TIME)
-					workHours=4
-					;;
+								workHours=4
+								;;
 			   *)
-					workHours=0
-					;;
+								workHours=0
+								;;
 	esac
 	echo $workHours
+}
+
+#FUNCTION-To Calculate DailyWage
+calDailyWage()
+{
+	local workHrs=$1;
+	wage=$(($workHrs*$EMP_RATE_PER_HR));
+	echo $wage
 }
 
 #Calculating Total working hrs by calling getWorkingHours()
@@ -34,7 +42,10 @@ do
 	((totalWorkingDays++))
 	workHours=$( getWorkingHours $((RANDOM%3)) )
 	totalWorkHours=$(($totalWorkHours+$workHours))
+	empWage[$totalWorkingDays]=$(calDailyWage $workHours)
 done
 
-#Total salary
-totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR));
+#Calculating Total salary
+totalSalary=$(calDailyWage $totalWorkHours);
+empWage+=${totalSalary}
+echo "Daily wage with total wage :" ${empWage[@]}
